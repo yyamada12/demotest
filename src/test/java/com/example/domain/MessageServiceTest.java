56410e6f9011d95ed98eced824a975893a3d4ee6
yyamada12 <12yacropolisy@gmail.com>
2021-03-01 23:33:24 +0900

    remove unneed annotation

--- src/test/java/com/example/domain/MessageServiceTest.java
+++ src/test/java/com/example/domain/MessageServiceTest.java
@@ -1,12 +1,10 @@
 package com.example.domain;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.stereotype.Service;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.core.Is.*;
 
-@Service
 public class MessageServiceTest {
     @Test
     public void testGetMessage() {
